f5d609da4ec16e4a12122c3c2226ffb921164132
Leonardo Arteaga <LeonardoADS08@hotmail.com>
2017-05-04 19:13:40 -0400

    Creación de los proyectos

---
+++ Arduino Sensor Data Analysis/SDA Arduino/SDA_Arduino.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		SDA_Arduino.cpp
+ Created:	04/05/2017 19:12:22
+ Author:	Leonardo
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "SDA_Arduino.h"
+
+
---
+++ Arduino Sensor Data Analysis/SDA Arduino/SDA_Arduino.h
@@ -0,0 +1,19 @@
+/*
+ Name:		SDA_Arduino.h
+ Created:	04/05/2017 19:12:22
+ Author:	Leonardo
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _SDA_Arduino_h
+#define _SDA_Arduino_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
