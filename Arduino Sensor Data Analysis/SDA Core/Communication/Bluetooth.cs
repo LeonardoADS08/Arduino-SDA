d261e9d1459eccc23831b9f6bb95845c368ac5da
Leonardo Arteaga <LeonardoADS08@hotmail.com>
2017-05-12 23:42:00 -0400

    Jerarquización clase communication

---
+++ Arduino Sensor Data Analysis/SDA Core/Communication/Bluetooth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA_Core.Communication
+{
+    class Bluetooth : Serial
+    {
+
+        public Bluetooth(string portName, int baudRate, int hearInterval = 100) : base(portName, baudRate, hearInterval)
+        {
+
+        }
+    }
+}
---
+++ Arduino Sensor Data Analysis/SDA Core/Communication/USB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA_Core.Communication
+{
+    class USB : Serial
+    {
+        public USB(string portName, int baudRate, int hearInterval = 100) : base(portName, baudRate, hearInterval)
+        {
+
+        }
+    }
+}
